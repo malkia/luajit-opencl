e7e899dfdbbe20e00d6690a2209bdf98a29893b2
Dimiter "malkia" Stanev <malkia@gmail.com>
2011-01-27 15:34:55 -0800

    Madness2

--- x.lua
+++ x.lua
@@ -6,19 +6,28 @@ local font = ffi.new( "uint8_t[?]", #font, font )
 
 local random = math.random
 local rshift, band = bit.rshift, bit.band
+local cos, sin = math.cos, math.sin
 
 local signal_noise = 0
 
-local vbo_capacity = 640*480*2
+local desktop_width = 0
+local desktop_height = 0
+local width = 640
+local height = 480
+
+local vbo_capacity = width*height*2
 local vbo_index = 0
 local vbo = ffi.new( "float[?]", vbo_capacity )
+
+local angle = 0
+local cos_angle = 0
+local sin_angle = 1
 
 local function draw_char(x,y,c)
    if random()*signal_noise > 0.25 then
       c = c + 1
    end
    c = c * 8
-   gl.glColor3f( signal_noise, 1 - signal_noise, random() )
    for i=0,7 do
       local b = font[c + i]
       for j=0,7 do
@@ -26,31 +35,46 @@ local function draw_char(x,y,c)
 	 if m == 1 and random() >= signal_noise then
 	    local x = x + j + random()*signal_noise
 	    local y = y + i + random()*signal_noise
-	    vbo[ vbo_index + 0 ] = x
-	    vbo[ vbo_index + 1 ] = y
+	    x = x - width / 2
+	    y = y - height / 2
+	    vbo[ vbo_index + 0 ] = (x * cos_angle - y * sin_angle) * (1.00 + signal_noise * random() / 4.0) * 1.4 + width / 2 
+	    vbo[ vbo_index + 1 ] = (y * cos_angle + x * sin_angle) * (1.00 + signal_noise * random() / 4.0) * 1.4 + height / 2
 	    vbo_index = vbo_index + 2
 	 end
       end
    end
 end
 
-local function draw_string(x,y,s)
---   gl.glBegin( gl.GL_POINTS )
+local function draw_char_normal(x,y,c)
+   c = c * 8
+   for i=0,7 do
+      local b = font[c + i]
+      for j=0,7 do
+	 local m = band(rshift(b, 7-j), 1)
+	 if m == 1 then
+	    vbo[ vbo_index + 0 ] = x + j
+	    vbo[ vbo_index + 1 ] = y + i
+	    vbo_index = vbo_index + 2
+	 end
+      end
+   end
+end
+
+local function draw_string(x,y,s,draw_char_function)
+   draw_char_function = draw_char_function or draw_char
    for i=1,#s do
       local c = s:byte(i)
       if c == 10 or c == 13 then
 	 y = y + 8 
-	 if y > 480 then
+	 if y > height then
 	    break
 	 end
 	 x = 0
       else
---	 draw_char(x+random(),y+random(),c)
-	 draw_char(x,y,c)
+	 draw_char_function(x,y,c)
 	 x = x + 8
       end
    end
---   gl.glEnd()
 end
 
 local function read_file(n)
@@ -60,31 +84,36 @@ end
 
 local text = read_file("x-font.lua")
 
+local fullscreen = false
+
 local function main()
    local frame = 0
    local c = 0
    assert( glfw.glfwInit() )
+   local desktop_mode = ffi.new( "GLFWvidmode[1]" )
+   glfw.glfwGetDesktopMode( desktop_mode )
+   desktop_width, desktop_height = desktop_mode[0].width, desktop_mode[0].height
+   local window_x = ( desktop_width - width ) / 2
+   local window_y = ( desktop_height - height ) / 2
    glfw.glfwOpenWindowHint(glfw.GLFW_WINDOW_NO_RESIZE, 1)
-   local window = glfw.glfwOpenWindow( 640, 480, glfw.GLFW_WINDOWED, "X", nil )
+   local window = glfw.glfwOpenWindow( width, height, glfw.GLFW_WINDOWED, "X", nil )
    assert( window )
-   glfw.glfwEnable(window, glfw.GLFW_STICKY_KEYS);
---   glfw.glfwSwapInterval(1);
+   glfw.glfwSetWindowPos(window, window_x, window_y)
+--   glfw.glfwEnable(window, glfw.GLFW_STICKY_KEYS)
+   glfw.glfwSwapInterval(0);
    local ct = glfw.glfwGetTime()
    local pt = glfw.glfwGetTime()
-   while glfw.glfwIsWindow(window) and glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) ~= glfw.GLFW_PRESS 
+   while glfw.glfwIsWindow(window) 
+   and   glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) ~= glfw.GLFW_PRESS 
    do
-      local x, y = ffi.new( "int[1]" ), ffi.new( "int[1]" )
-      glfw.glfwGetMousePos(window, x, y)
-      x, y = x[0], y[0]
-
-      local width, height = ffi.new( "int[1]" ), ffi.new( "int[1]" )
-      glfw.glfwGetWindowSize(window, width, height);
-      width, height = width[0], height[0]
-
-      gl.glViewport(0, 0, width, height);
-      gl.glClearColor(0, 0, 0, 0);
+      pt, ct = ct, glfw.glfwGetTime()
+      frame = frame + 1
+      
+      gl.glPointSize(random()*signal_noise)
+      
+      gl.glColor3f( signal_noise, 1 - signal_noise, random() )
       if random() * signal_noise < 0.30 then
-      gl.glClear(gl.GL_COLOR_BUFFER_BIT);
+	 gl.glClear(gl.GL_COLOR_BUFFER_BIT);
       end
 
       gl.glMatrixMode(gl.GL_PROJECTION);
@@ -94,17 +123,8 @@ local function main()
       gl.glMatrixMode( gl.GL_MODELVIEW );
       gl.glLoadIdentity();
  
-      if random() * signal_noise > 0.75 then
-	 gl.glBegin(gl.GL_POLYGON);
-	 gl.glVertex3f(0.25*width, 0.25*height, 0.0);
-	 gl.glVertex3f(0.75*width, 0.25*height, 0.0);
-	 gl.glVertex3f(0.75*width, 0.75*height, 0.0);
-	 gl.glVertex3f(0.25*width, 0.75*height, 0.0);
-	 gl.glEnd();
-      end
-
-      draw_string( 0.5, 0.5, tostring(ct-pt).."\n"..string.sub(text,math.mod(0,#text)))
-      frame = frame + 1
+      draw_string( 0.5, 0.5, tostring(ct-pt), draw_char_normal )
+      draw_string( 0.5, 0.5 + 10, text )
 
       signal_noise = signal_noise + (random() - 0.5) / 10.0
       if signal_noise < 0 then
@@ -118,11 +138,30 @@ local function main()
       gl.glEnableClientState(gl.GL_VERTEX_ARRAY);
       gl.glVertexPointer(2, gl.GL_FLOAT, 0, vbo);
       gl.glDrawArrays(gl.GL_POINTS, 0, vbo_index);
+      gl.glDisableClientState(gl.GL_VERTEX_ARRAY);
       vbo_index = 0
+      
+      angle = angle + signal_noise / 10.0 * random() - 0.01
+      cos_angle = cos( angle )
+      sin_angle = sin( angle )
 
+      if glfw.glfwGetKey(window, glfw.GLFW_KEY_SPACE) == glfw.GLFW_PRESS then
+	 print(fullscreen)
+	 fullscreen = not fullscreen
+	 if fullscreen then
+	        new_window = glfw.glfwOpenWindow( width, height, glfw.GLFW_FULLSCREEN, "X", window )
+                glfw.glfwSwapInterval(1);
+	 else
+	        new_window = glfw.glfwOpenWindow( width, height, glfw.GLFW_WINDOWED, "X", window )
+                glfw.glfwSetWindowPos(new_window, window_x, window_y)
+                glfw.glfwSwapInterval(0);
+	 end
+	 glfw.glfwCloseWindow(window)
+	 window = new_window
+      end
+      
       glfw.glfwSwapBuffers();
       glfw.glfwPollEvents();
-      pt, ct = ct, glfw.glfwGetTime()
    end
    glfw.glfwTerminate();
 end
